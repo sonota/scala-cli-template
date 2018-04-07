9ef76dbb8b6bac227cc74c6ace1d5c97e6282197
sonota88 <yosiot8753@gmail.com>
2018-04-07 11:00:33 +0900

    Add Maparse

---
+++ src/main/scala/sample/Maparse.scala
@@ -0,0 +1,41 @@
+package sample
+
+object Maparse {
+
+  def parseOptionals(optionalArgs: Array[String]): Map[String, String] = {
+    var opts: Map[String, String] = Map()
+
+    optionalArgs.foreach{ arg =>
+      val idx = arg.indexOf("=")
+
+      if (idx < 0) {
+        opts = opts + ( arg -> "true" )
+      } else {
+        val k = arg.substring(0, idx)
+        val v = arg.substring(idx + 1)
+        opts = opts + (k -> v)
+      }
+    }
+
+    opts
+  }
+
+  def parseArgs(args: Array[String], names: String*): Map[String, String] = {
+    var opts: Map[String, String] = Map()
+
+    if (args.length == names.length) {
+      // no optional arguments
+    } else if (args.length > names.length) {
+      opts = parseOptionals(args.drop(names.length))
+    } else {
+      throw new IllegalArgumentException()
+    }
+
+    names.zipWithIndex.foreach{ case(name, i) =>
+      opts = opts + (name -> args(i))
+    }
+
+    opts
+  }
+
+}
